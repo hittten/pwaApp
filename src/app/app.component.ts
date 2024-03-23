ac9f3ac61c6eeeb1751c143488c71d67602602ef
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2024-03-23 22:56:23 +0100

    implement pwa service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,39 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pwaApp';
+  title = 'v1.0.0';
+  private updates = inject(SwUpdate)
+
+  constructor() {
+    this.updates
+      .versionUpdates.subscribe((event) => {
+      switch (event.type) {
+        case 'NO_NEW_VERSION_DETECTED':
+          console.log('PWA: already on the latest version')
+          break;
+        case 'VERSION_DETECTED':
+          console.log(`PWA: new version is available.`);
+          console.log(`PWA: downloading new app version: ${event.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`PWA: current app version: ${event.currentVersion.hash}`);
+          console.log(`PWA: app version ready for use: ${event.latestVersion.hash}`);
+
+          if (confirm("new version, update?")) {
+            document.location.reload()
+          }
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          // TODO: installations fail always
+          console.log(`PWA: failed to install app version '${event.version.hash}': ${event.error}`);
+          console.error("PWA: failed to install update to version", event.version, event.error)
+          break;
+      }
+    })
+
+    this.updates.unrecoverable.subscribe(async event => {
+      console.error('An error occurred that we cannot recover from:', event.reason)
+      alert('An error occurred that we cannot recover from:' + event.reason + '\n\nPlease reload the page.')
+    })
+  }
 }
